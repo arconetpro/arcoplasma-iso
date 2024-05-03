e693240100b36dc825920ef696f0f7a55b1c5853
arcolinuxz <arcolinuxinfo@gmail.com>
2024-05-03 11:28:27 +0200

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -15,11 +15,12 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=(gzip -cn9)
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:600"
   ["/root"]="0:0:700"
+  ["/root/.gnupg"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
