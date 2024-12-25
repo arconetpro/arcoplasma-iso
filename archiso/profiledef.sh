9297b804df4d5af35c8baeef6bf66a104bbcb718
arcolinuxz <arcolinuxinfo@gmail.com>
2024-12-25 09:41:36 +0100

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="arcoplasma"
-iso_label="arcoplasma-v25.01.02"
+iso_label="arcoplasma-v25.02.01"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v25.01.02"
+iso_version="v25.02.01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
